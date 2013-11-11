cb0f756f7fef66483adda5514a509ee2db5bd27b
Seg <seg@segonmedia.com>
2013-11-10 22:23:14 -0800

    Added svgmin for Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,6 +180,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-svgmin');
   
   // Inital Setup Task
   grunt.registerTask( 'init', [ 'init' , 'build' ] );
